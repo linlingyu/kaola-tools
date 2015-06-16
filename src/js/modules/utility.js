b808e6e7da33f9d312201c551f4a564a0487bd89
米德 <lingyu.lly@alibaba-inc.com>
2015-06-16 18:44:19 +0800

    by lly

--- src/js/modules/utility.js
+++ src/js/modules/utility.js
@@ -1,9 +1,18 @@
 define(function(require, exports, module){
     module.exports = {
         defineProperties: function(obj, methods, options){
-            var c = {}, item;
+            var c = {}, item, getter, setter;
             for(var i in methods){
-                item = c[i] = {value: methods[i]};
+                if(!methods.hasOwnProperty(i)){continue;}
+                item = c[i] = {};
+                getter = methods.__lookupGetter__(i);
+                setter = methods.__lookupSetter__(i);
+                if (!getter && !setter) {
+                    item.value = methods[i];
+                } else {
+                    getter && (item.get = getter);
+                    setter && (item.set = setter);
+                }
                 if(options){
                     for(var j in options[i]){
                         item[j] = options[i][j];
@@ -14,9 +23,18 @@ define(function(require, exports, module){
         },
 
         extend: function(source, copy){
+            var getter, setter;
             if(copy){
                 for(var i in copy){
-                    source[i] = copy[i];
+                    if (!copy.hasOwnProperty(i)){continue;}
+                    getter = copy.__lookupGetter__(i);
+                    setter = copy.__lookupSetter__(i);
+                    if (!getter && !setter) {
+                        source[i] = copy[i];
+                    } else {
+                        getter && copy.__defineGetter__(i, getter);
+                        setter && copy.__defineSetter__(i, setter);
+                    }
                 }
             }
             return source;
